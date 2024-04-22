7b2365ec763492a3c31bdf7d96dc35ed7d86bf5c
F-0728 <fugue2767@icloud.com>
2024-04-22 22:46:49 +0900

    added power func

--- C++/utilities.cpp
+++ C++/utilities.cpp
@@ -1,7 +1,12 @@
 #include <atcoder/all>
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 using namespace std;
 using namespace atcoder;
+
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 
 string baseNtoM(string &s, int n, int m) {
     if (s == "0") {
@@ -18,4 +23,16 @@ string baseNtoM(string &s, int n, int m) {
     }
     std::reverse(r.begin(), r.end());
     return r;
+}
+
+ll power(ll x, ll y) {
+    ll res = 1;
+    while (y > 0) {
+        if (y % 2 == 1) {
+            res *= x;
+        }
+        x *= x;
+        y >>= 1;
+    }
+    return res;
 }
