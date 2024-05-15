35a9abf7d0059f0507c9092c3502f4ee54097bbd
F-0728 <fugue2767@icloud.com>
2024-05-15 11:14:07 +0900

    added vll and vvll

--- template.cpp
+++ template.cpp
@@ -10,6 +10,8 @@ using mint = modint1000000007;
 // using mint = modint998244353;
 using ll = long long;
 #define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
 #define Yes cout << "Yes" << endl;
 #define No cout << "No" << endl;
 
