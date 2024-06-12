e0df901cb012153f68ef46a7ff02b3d1a69cdc0b
F-0728 <fugue2767@icloud.com>
2024-06-12 11:24:47 +0900

    added inf / INF

--- template.cpp
+++ template.cpp
@@ -9,6 +9,8 @@ using namespace atcoder;
 using mint = modint1000000007;
 // using mint = modint998244353;
 using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
 #define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 #define vll vector<ll>
 #define vvll vector<vector<ll>>
