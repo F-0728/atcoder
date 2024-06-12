48c97cdb119a937092d0a53011173a032a0d573a
F-0728 <fugue2767@icloud.com>
2024-06-12 15:23:08 +0900

    solved ABC194-D

---
+++ ABC/194D.cpp
@@ -0,0 +1,31 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+using namespace std;
+using namespace atcoder;
+
+using mint = modint1000000007;
+// using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+
+int main() {
+    ll N;
+    cin >> N;
+    double ans = 0;
+    for (ll i = 1; i < N; i++) {
+        ans += (double)N / (N - i);
+    }
+    cout << fixed << setprecision(10) << ans << endl;
+
+    return 0;
+}
