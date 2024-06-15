7e3c61d7cae1e32a799dddf08e410427821ac13c
F-0728 <fugue2767@icloud.com>
2024-06-16 02:32:17 +0900

    solved EDPC-B

---
+++ EDPC/B.cpp
@@ -0,0 +1,41 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+int main() {
+    ll N, K;
+    cin >> N >> K;
+    vll height(N);
+    rep(i, N) cin >> height.at(i);
+    vll dp(N, INF);
+    dp.at(0) = 0;
+    rep(i, N) {
+        if (i == 0) continue;
+        rep(j, K + 1) {
+            if (i - j < 0) continue;
+            dp.at(i) = min(dp.at(i), dp.at(i - j) + abs(height.at(i) - height.at(i - j)));
+        }
+    }
+    cout << dp.at(N - 1) << endl;
+
+    return 0;
+}
