0e4be5cc683e7289efee0fc9c3ee512f3e977b8d
F-0728 <fugue2767@icloud.com>
2024-07-24 23:29:41 +0900

    solved ABC336-D

---
+++ ABC/D/336D.cpp
@@ -0,0 +1,45 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+int main() {
+    ll N;
+    cin >> N;
+    vll A(N + 2);
+    rep(i, N) cin >> A.at(i + 1);
+    vll m(N);
+    vll M(N);
+    ll ans = -INF;
+    rep(i, N - 1) {
+        m.at(i + 1) = min(m.at(i), A.at(i + 2) - i - 2);
+    }
+    M.at(N - 1) = N + 1;
+    for (ll i = N - 2; i >= 0; i--) {
+        M.at(i) = min(M.at(i + 1), A.at(i + 2) + i + 2);
+    }
+    rep(i, N) {
+        ans = max(ans, min(m.at(i) + i + 1, M.at(i) - i - 1));
+    }
+    cout << ans << endl;
+
+    return 0;
+}
