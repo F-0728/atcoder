8551fc33ba489daecc9e81c92d65e63c848a58fc
F-0728 <fugue2767@icloud.com>
2024-08-11 08:57:39 +0900

    solved tessoku A68: ford-fulkerson

---
+++ tessoku/A68.cpp
@@ -0,0 +1,89 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+struct Edge { 
+    ll rev, from, to, cap;
+};
+
+class FordFulkerson {
+public:
+    vector<vector<Edge>> G;
+    vector<bool> visited;
+
+    ll size = 0;
+    void init(ll n) {
+        G.resize(n);
+        visited.resize(n);
+        size = n;
+    }
+
+    void add_edge(ll u, ll v, ll cost) {
+        ll u_vID = G.at(u).size();
+        ll v_uID = G.at(v).size();
+        G.at(u).emplace_back(Edge{v_uID, u, v, cost});
+        G.at(v).emplace_back(Edge{u_vID, v, u, 0});
+    }
+
+    ll dfs(ll pos, ll goal, ll F) {
+        if (pos == goal) return F;
+        visited.at(pos) = true;
+        for(Edge &e : G.at(pos)){
+            if (e.cap == 0 or visited.at(e.to)) continue;
+            ll flow = dfs(e.to, goal, min(F, e.cap));
+            if (flow > 0) {
+                e.cap -= flow;
+                G.at(e.to).at(e.rev).cap += flow;
+                return flow;
+            }
+        }
+        return 0;
+    }
+
+    ll maxFlow(ll s, ll t) {
+        ll totalFlow = 0;
+        while (1) {
+            visited.assign(size, false);
+            ll F = dfs(s, t, INF);
+            if (F == 0) break;
+            totalFlow += F;
+        }
+        return totalFlow;
+    }
+};
+
+int main() {
+    ll N, M;
+    cin >> N >> M;
+    
+    FordFulkerson ff;
+    ff.init(N);
+    rep(i, M) {
+        ll A, B, C;
+        cin >> A >> B >> C;
+        A--; B--;
+        ff.add_edge(A, B, C);
+    }
+    cout << ff.maxFlow(0, N - 1) << endl;
+
+    return 0;
+}
