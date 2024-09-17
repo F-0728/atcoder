51b8a9b762532dcaafa9643ee4899894d10e0894
F-0728 <fugue2767@icloud.com>
2024-09-17 16:26:23 +0900

    added dir to template

--- template.cpp
+++ template.cpp
@@ -12,6 +12,7 @@ using mint = modint998244353;
 using ll = long long;
 const ll inf = 1 << 30;
 const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 #define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
 #define vll vector<ll>
 #define pll pair<ll, ll>
