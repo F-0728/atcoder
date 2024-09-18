1c582574b80d3080884551895c646318528b7fe2
F-0728 <fugue2767@icloud.com>
2024-09-18 15:03:46 +0900

    solved ABC052-D

---
+++ ABC/D/52D.cpp
@@ -0,0 +1,39 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+int main() {
+    ll N, A, B;
+    cin >> N >> A >> B;
+    vll X(N);
+    rep(i, N) cin >> X[i];
+
+    ll ans = 0;
+    rep(i, N - 1) {
+        ans += min((X[i + 1] - X[i]) * A, B);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
