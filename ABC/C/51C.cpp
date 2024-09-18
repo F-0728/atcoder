3d7ba38d4e2bb27c15a41c244cc978ac7bcd2a74
F-0728 <fugue2767@icloud.com>
2024-09-18 15:08:42 +0900

    solved ABC051-C

---
+++ ABC/C/51C.cpp
@@ -0,0 +1,48 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+int main() {
+    ll sx, sy, tx, ty;
+    cin >> sx >> sy >> tx >> ty;
+
+    string ans = "";
+    rep(i, tx - sx) ans += "R";
+    rep(i, ty - sy) ans += "U";
+    rep(i, tx - sx) ans += "L";
+    rep(i, ty - sy) ans += "D";
+
+    ans += "D";
+
+    rep(i, tx - sx + 1) ans += "R";
+    rep(i, ty - sy + 1) ans += "U";
+    ans += "L";
+    ans += "U";
+    rep(i, tx - sx + 1) ans += "L";
+    rep(i, ty - sy + 1) ans += "D";
+    ans += "R";
+
+    cout << ans << endl;
+
+    return 0;
+}
