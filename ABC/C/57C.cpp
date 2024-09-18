6363d02fa97a33897cc5983355c87e087f7982fd
F-0728 <fugue2767@icloud.com>
2024-09-18 13:54:01 +0900

    solved ABC057-C

---
+++ ABC/C/57C.cpp
@@ -0,0 +1,49 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+vector<ll> divisors(ll x) {
+    vector<ll> divs;
+    for (ll i = 1; i * i <= x; i++) {
+        if (x % i == 0) {
+            divs.push_back(i);
+            if (i * i != x) {
+                divs.push_back(x / i);
+            }
+        }
+    }
+    sort(divs.begin(), divs.end());
+    return divs;
+}
+
+int main() {
+    ll N;
+    cin >> N;
+
+    vll divs = divisors(N);
+    ll a = divs[divs.size() / 2];
+    
+    cout << to_string(a).size() << endl;
+
+    return 0;
+}
