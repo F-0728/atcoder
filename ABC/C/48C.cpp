9f780209d26bc82c2bcf521b8829a29b4c9707e3
F-0728 <fugue2767@icloud.com>
2024-09-18 15:47:13 +0900

    solved ABC048-C

---
+++ ABC/C/48C.cpp
@@ -0,0 +1,49 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+int main() {
+    ll N, x;
+    cin >> N >> x;
+
+    vll a(N);
+    rep(i, N) cin >> a[i];
+
+    ll ans = 0;
+    for (ll i = 1; i < N; i++) {
+        if (a[i] + a[i - 1] > x) {
+            ll diff = a[i] + a[i - 1] - x;
+            ans += diff;
+            if (a[i] >= diff) {
+                a[i] -= diff;
+            } else {
+                a[i - 1] -= diff - a[i];
+                a[i] = 0;
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
