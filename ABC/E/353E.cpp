72794cff580cc94dfb6a3704fda1073ecd1db5c6
F-0728 <fugue2767@icloud.com>
2024-09-20 11:11:00 +0900

    solved ABC353-E: Trie

---
+++ ABC/E/353E.cpp
@@ -0,0 +1,124 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+/* Trie 木： 文字の種類(char_size)、int型で0に対応する文字(base)
+    insert(word): 単語 word を Trie 木に挿入する
+    search(word): 単語 word が Trie 木にあるか判定する
+    start_with(prefix):  prefix が一致する単語が Trie 木にあるか判定する
+    count(): 挿入した単語の数を返す
+    size(): Trie 木の頂点数を返す
+    計算量：insert, search ともに O(M)（Mは単語の長さ）
+*/
+template <int char_size, int base>
+struct Trie {
+    struct Node {            // 頂点を表す構造体
+        vector<int> next;    // 子の頂点番号を格納。存在しなければ-1
+        vector<int> accept;  // 末端がこの頂点になる単語の word_id を保存
+        int c;               // base からの間隔をint型で表現したもの
+        ll common;          // いくつの単語がこの頂点を共有しているか
+        Node(int c_) : c(c_), common(0) {
+            next.assign(char_size, -1);
+        }
+    };
+
+    vector<Node> nodes;  // trie 木本体
+    int root;
+    Trie() : root(0) {
+        nodes.push_back(Node(root));
+    }
+
+    // 単語の挿入
+    void insert(const string &word, int word_id) {
+        int node_id = 0;
+        for (int i = 0; i < (int)word.size(); i++) {
+            int c = (int)(word[i] - base);
+            int &next_id = nodes[node_id].next[c];
+            if (next_id == -1) {  // 次の頂点が存在しなければ追加
+                next_id = (int)nodes.size();
+                nodes.push_back(Node(c));
+            }
+            ++nodes[node_id].common;
+            node_id = next_id;
+        }
+        ++nodes[node_id].common;
+        nodes[node_id].accept.push_back(word_id);
+    }
+    void insert(const string &word) {
+        insert(word, nodes[0].common);
+    }
+
+    // 単語とprefixの検索
+    bool search(const string &word, bool prefix = false) {
+        int node_id = 0;
+        for (int i = 0; i < (int)word.size(); i++) {
+            int c = (int)(word[i] - base);
+            int &next_id = nodes[node_id].next[c];
+            if (next_id == -1) {  // 次の頂点が存在しなければ終了
+                return false;
+            }
+            node_id = next_id;
+        }
+        return (prefix) ? true : nodes[node_id].accept.size() > 0;
+    }
+
+    // prefix を持つ単語が存在するかの検索
+    bool start_with(const string &prefix) {
+        return search(prefix, true);
+    }
+
+    // 挿入した単語の数
+    int count() const {
+        return (nodes[0].common);
+    }
+    // Trie木のノード数
+    int size() const {
+        return ((int)nodes.size());
+    }
+};
+
+int main() {
+    ll N;
+    cin >> N;
+    vector<string> S(N);
+    rep(i, N) cin >> S[i];
+
+    Trie<26, 'a'> trie;
+    rep(i, N) {
+        trie.insert(S[i]);
+    }
+
+    ll ans = 0;
+    ll M = trie.size();
+    rep(i, M) {
+        // cout << i << " " << trie.nodes[i].common << endl;
+        if (i == 0) continue;
+        auto node = trie.nodes[i];
+        ans += node.common * (node.common - 1) / 2;
+    }
+
+    cout << ans << endl;
+
+
+    return 0;
+}
