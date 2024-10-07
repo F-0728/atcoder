eb848009efb49d891a60008c36546fd3487b1959
F-0728 <fugue2767@icloud.com>
2024-10-07 21:53:48 +0900

    solved CodeQUEEN2024-A

---
+++ other-contests/CodeQUEEN2024/A.cpp
@@ -0,0 +1,37 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+int main() {
+    ll X;
+    cin >> X;
+    if (X <= 10) {
+        cout << 10 << endl;
+    } else if (X <= 15) {
+        cout << 15 << endl;
+    } else {
+        cout << 17 << endl;
+    }
+
+    return 0;
+}
