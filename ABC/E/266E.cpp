3eb5311d594e23bf7c1d7df41990fe73c4d081a4
F-0728 <fugue2767@icloud.com>
2024-10-25 15:38:59 +0900

    solved ABC266-E

---
+++ ABC/E/266E.cpp
@@ -0,0 +1,45 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+int main() {
+    ll N;
+    cin >> N;
+
+    ld ans = 3.5;
+
+    rep(i, N - 1) {
+        ld res = max((ld)1, ans) + max((ld)2, ans) + max((ld)3, ans) + max((ld)4, ans) + max((ld)5, ans) + max((ld)6, ans);
+        ans = res / 6;
+    }
+
+    cout << fixed << setprecision(10) << ans << endl;
+
+
+    return 0;
+}
