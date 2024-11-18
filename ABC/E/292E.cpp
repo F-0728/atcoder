56d3bc63485f2a9dc076b22f8e677ec72684c2b4
F-0728 <fugue2767@icloud.com>
2024-11-18 15:17:05 +0900

    solved ABC292-E with editorial

---
+++ ABC/E/292E.cpp
@@ -0,0 +1,99 @@
+#include <atcoder/all>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+ll power(ll x, ll y) {
+    ll res = 1;
+    while (y > 0) {
+        if (y % 2 == 1) {
+            res *= x;
+        }
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+template <typename T>
+void outv(const vector<T>& v) {
+    for (const auto& e : v) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    ll N, M;
+    cin >> N >> M;
+    vvll G(N);
+    rep(i, M) {
+        ll a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        G[a].push_back(b);
+    }
+    ll ans = 0;
+    rep(i, N) {
+        vbool visited(N, false);
+        queue<ll> q;
+        q.push(i);
+        while (!q.empty()) {
+            ll now = q.front();
+            q.pop();
+            if (visited[now]) {
+                continue;
+            }
+            visited[now] = true;
+            for (auto next : G[now]) {
+                if (visited[next]) {
+                    continue;
+                }
+                q.push(next);
+            }
+        }
+        ll res = 0;
+        rep(j, N) {
+            if (visited[j]) {
+                res++;
+            }
+        }
+        ans += res - 1;
+    }
+
+    cout << ans - M << endl;
+
+    return 0;
+}
