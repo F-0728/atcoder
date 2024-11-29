7e08efe8d34c40f4035c079834a23bff62043ce0
F-0728 <fugue2767@icloud.com>
2024-11-29 10:13:57 +0900

    solved ABC275-E

---
+++ ABC/E/275E.cpp
@@ -0,0 +1,70 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <chrono>
+#include <bitset>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+ll power(ll x, ll y) {ll res = 1; while(y > 0){if (y % 2 == 1) {res *= x;} x *= x; y >>= 1;}return res;}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+
+int main() {
+    ll N, M, K;
+    cin >> N >> M >> K;
+
+    mint invM = mint(M).inv();
+
+    vmint p(N + 1, 0);
+    p[0] = 1;
+
+    mint ans = 0;
+    rep(i, K) {
+        vmint np(N + 1, 0);
+        rep(j, N) {
+            for (ll k = 1; k <= M; k++) {
+                if (p[j] == 0) continue;
+                if (j + k <= N) {
+                    np[j + k] += p[j] * invM;
+                } else {
+                    np[N - (j + k - N)] += p[j] * invM;
+                }
+            }
+        }
+        p = np;
+        ans += p[N];
+    }
+
+    cout << ans.val() << endl;
+
+    return 0;
+}
