e6cdc2e54fcca04e78947b1b9200198e9ccf4958
F-0728 <fugue2767@icloud.com>
2024-12-06 16:22:20 +0900

    solved ABC280-E

---
+++ ABC/E/280E.cpp
@@ -0,0 +1,75 @@
+#include <atcoder/all>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+ll power(ll x, ll y) {
+    ll res = 1;
+    while (y > 0) {
+        if (y % 2 == 1) {
+            res *= x;
+        }
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+template <typename T>
+void outv(const vector<T>& v) {
+    for (const auto& e : v) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    ll N;
+    cin >> N;
+    ll P;
+    cin >> P;
+
+    mint p = (mint)P / 100;
+    mint q = 1 - p;
+
+    vmint dp(N + 1, mint(0));
+    dp[1] = 1;
+    for (ll i = 2; i <= N; i++) {
+        dp[i] = (dp[i - 1] * q + dp[i - 2] * p) + 1;
+    }
+
+    cout << dp[N].val() << endl;
+
+    return 0;
+}
