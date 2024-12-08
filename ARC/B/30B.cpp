0f1957b9b71eab162fae6e384dff8d9f5477689b
F-0728 <fugue2767@icloud.com>
2024-12-08 17:35:28 +0900

    solved ARC030-B

---
+++ ARC/B/30B.cpp
@@ -0,0 +1,118 @@
+#include <atcoder/all>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+ll power(ll x, ll y) {
+    ll res = 1;
+    while (y > 0) {
+        if (y % 2 == 1) {
+            res *= x;
+        }
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+template <typename T>
+void outv(const vector<T>& v) {
+    for (const auto& e : v) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+vvll G;
+vll H;
+vbool hasH;
+bool dfs(ll v, ll p) {
+    bool res = false;
+    if (H[v]) {
+        res = true;
+    }
+    for (auto nv : G[v]) {
+        if (nv == p) {
+            continue;
+        }
+        res |= dfs(nv, v);
+    }
+    hasH[v] = res;
+    return res;
+}
+
+int main() {
+    ll N, X;
+    cin >> N >> X;
+    H.resize(N);
+    rep(i, N) { cin >> H[i]; }
+
+    G.resize(N);
+    rep(i, N - 1) {
+        ll A, B;
+        cin >> A >> B;
+        A--;
+        B--;
+        G[A].push_back(B);
+        G[B].push_back(A);
+    }
+
+    ll ans = 0;
+    ll start = X - 1;
+    hasH.resize(N, false);
+    dfs(start, -1);
+    ll count = 0;
+    queue<pll> q;
+    q.push({start, -1});
+    while (!q.empty()) {
+        ll v = q.front().first;
+        ll p = q.front().second;
+        // cout << v << " " << p << endl;
+        q.pop();
+        if (!hasH[v]) continue;
+        for (auto nv : G[v]) {
+            if (nv == p) {
+                continue;
+            }
+            if (hasH[nv]) {
+                ans += 2;
+                q.push({nv, v});
+            }
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
