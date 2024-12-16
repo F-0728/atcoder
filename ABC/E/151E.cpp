f4fb4cea6acc1ca270fd25c0c70d6ffeaed7c167
F-0728 <fugue2767@icloud.com>
2024-12-16 15:06:39 +0900

    solved ABC151-E

---
+++ ABC/E/151E.cpp
@@ -0,0 +1,103 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <chrono>
+#include <bitset>
+using namespace std;
+using namespace atcoder;
+
+using mint = modint1000000007;
+// using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+ll power(ll x, ll y) {ll res = 1; while(y > 0){if (y % 2 == 1) {res *= x;} x *= x; y >>= 1;}return res;}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+void outvmint(const vector<mint>& v){for(const auto& e : v){ cout << e.val() << " "; } cout << endl;}
+template<typename T> void outvv(const vector<vector<T>>& vv){for(const auto& v : vv){ outv(v); }}
+void outvvmint(const vector<vector<mint>>& vv){for(const auto& v : vv){ outvmint(v); }}
+template<typename T> void outvp(const vector<pair<T, T>>& vp){for(const auto& p : vp){ cout << p.first << " " << p.second << endl; }}
+
+const ll MAX = 2000005;
+mint fact[MAX], factInv[MAX], inv[MAX];
+
+void init() {
+    ll MOD = mint::mod();
+    fact[0] = fact[1] = 1;
+    factInv[0] = factInv[1] = 1;
+    inv[1] = 1;
+    for (ll i = 2; i < MAX; i++) {
+        fact[i] = fact[i - 1] * i;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i);
+        factInv[i] = factInv[i - 1] * inv[i];
+    }
+}
+
+mint nPr(ll n, ll r) {
+    if (n < r) return 0;
+    if (n < 0 || r < 0) return 0;
+    return fact[n] * factInv[n - r];
+}
+
+mint nCr(ll n, ll r) {
+    if (n < r) return 0;
+    if (n < 0 || r < 0) return 0;
+    return fact[n] * factInv[r] * factInv[n - r];
+}
+
+int main() {
+    ll N, K;
+    cin >> N >> K;
+    vll A(N);
+    rep(i, N) {
+        cin >> A[i];
+    }
+    sort(A.begin(), A.end());
+
+    vmint maxComb(N, 0);
+    vmint minComb(N, 0);
+    init();
+    rep(i, N) {
+        if (i >= K - 1) {
+            maxComb[i] = nCr(i, K - 1);
+        }
+        if (i <= N - K) {
+            minComb[i] = nCr(N - i - 1, K - 1);
+        }
+    }
+
+    mint ans = 0;
+    rep(i, N) {
+        ans += maxComb[i] * A[i];
+        ans -= minComb[i] * A[i];
+    }
+
+    cout << ans.val() << endl;
+
+
+    return 0;
+}
