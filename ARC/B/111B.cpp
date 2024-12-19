160597d9f3f2bb1b0f4b763d78d2559fc2662485
F-0728 <fugue2767@icloud.com>
2024-12-19 10:38:00 +0900

    solved ARC111-B

---
+++ ARC/B/111B.cpp
@@ -0,0 +1,148 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <chrono>
+#include <bitset>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+ll power(ll x, ll y) {ll res = 1; while(y > 0){if (y % 2 == 1) {res *= x;} x *= x; y >>= 1;}return res;}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+void outvmint(const vector<mint>& v){for(const auto& e : v){ cout << e.val() << " "; } cout << endl;}
+template<typename T> void outvv(const vector<vector<T>>& vv){for(const auto& v : vv){ outv(v); }}
+void outvvmint(const vector<vector<mint>>& vv){for(const auto& v : vv){ outvmint(v); }}
+template<typename T> void outvp(const vector<pair<T, T>>& vp){for(const auto& p : vp){ cout << p.first << " " << p.second << endl; }}
+
+template< typename T >
+struct Compress {
+  vector< T > xs;
+
+  Compress() = default;
+
+  Compress(const vector< T > &vs) {
+    add(vs);
+  }
+
+  Compress(const initializer_list< vector< T > > &vs) {
+    for(auto &p : vs) add(p);
+  }
+
+  void add(const vector< T > &vs) {
+    copy(begin(vs), end(vs), back_inserter(xs));
+  }
+
+  void add(const T &x) {
+    xs.emplace_back(x);
+  }
+
+  void build() {
+    sort(begin(xs), end(xs));
+    xs.erase(unique(begin(xs), end(xs)), end(xs));
+  }
+
+  vector< int > get(const vector< T > &vs) const {
+    vector< int > ret;
+    transform(begin(vs), end(vs), back_inserter(ret), [&](const T &x) {
+      return lower_bound(begin(xs), end(xs), x) - begin(xs);
+    });
+    return ret;
+  }
+
+  int get(const T &x) const {
+    return lower_bound(begin(xs), end(xs), x) - begin(xs);
+  }
+
+  const T &operator[](int k) const {
+    return xs[k];
+  }
+};
+
+
+int main() {
+    ll N;
+    cin >> N;
+    ll M = 1;
+    dsu d(4e5 + 1);
+    vvll G(4e5 + 1);
+    rep(i, N) {
+        ll a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        d.merge(a, b);
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    ll ans = 0;
+    auto groups = d.groups();
+    for (auto group : groups) {
+        bool isTree = true;
+        Compress<int> cp(group);
+        cp.build();
+        ll n = cp.xs.size();
+        vbool visited(n, false);
+        queue<pll> q;
+        q.push({cp.get(group[0]), -1});
+        while (!q.empty()) {
+            auto p = q.front();
+            q.pop();
+            ll v = p.first;
+            ll prev = p.second;
+            if (visited[v]) {
+                isTree = false;
+                break;
+            }
+            visited[v] = true;
+            for (auto next : G[group[v]]) {
+                ll nv = cp.get(next);
+                if (nv == prev) continue;
+                if (visited[nv]) {
+                    isTree = false;
+                    break;
+                }
+                q.push({nv, v});
+            }
+        }
+        if (isTree) {
+            ans += n - 1;
+        } else {
+            ans += n;
+        }
+    }
+
+    cout << ans << endl;
+
+
+
+    return 0;
+}
