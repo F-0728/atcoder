686b2930a50c0dc51857b98c03c74a4c5cee9337
F-0728 <fugue2767@icloud.com>
2024-12-20 15:19:33 +0900

    solved ABC120-D

---
+++ ABC/D/120D.cpp
@@ -0,0 +1,78 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <chrono>
+#include <bitset>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+ll power(ll x, ll y) {ll res = 1; while(y > 0){if (y % 2 == 1) {res *= x;} x *= x; y >>= 1;}return res;}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+void outvmint(const vector<mint>& v){for(const auto& e : v){ cout << e.val() << " "; } cout << endl;}
+template<typename T> void outvv(const vector<vector<T>>& vv){for(const auto& v : vv){ outv(v); }}
+void outvvmint(const vector<vector<mint>>& vv){for(const auto& v : vv){ outvmint(v); }}
+template<typename T> void outvp(const vector<pair<T, T>>& vp){for(const auto& p : vp){ cout << p.first << " " << p.second << endl; }}
+
+int main() {
+    ll N, M;
+    cin >> N >> M;
+
+    vpll edges(M);
+    rep(i, M) {
+        ll u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        edges[i] = {u, v};
+    }
+    reverse(edges.begin(), edges.end());
+
+    dsu d(N);
+    vll g(M + 1);
+    g[0] = N * (N - 1) / 2;
+    rep(i, M) {
+        ll u = edges[i].first;
+        ll v = edges[i].second;
+        if (d.same(u, v)) {
+            g[i + 1] = g[i];
+        } else {
+            g[i + 1] = g[i] - d.size(u) * d.size(v);
+            d.merge(u, v);
+        }
+    }
+    reverse(g.begin(), g.end());
+    rep(i, M) {
+        cout << g[i + 1] << endl;
+    }
+
+    return 0;
+}
