1ecc34ebe43686f8ece03b1fe67c318c75c6cfb6
F-0728 <fugue2767@icloud.com>
2025-01-19 00:03:44 +0900

    solved ABC210-E

---
+++ ABC/E/210E.cpp
@@ -0,0 +1,75 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <chrono>
+#include <bitset>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << endl
+#define No cout << "No" << endl
+ll power(ll x, ll y) {ll res = 1; while(y > 0){if (y % 2 == 1) {res *= x;} x *= x; y >>= 1;}return res;}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << endl;}
+void outvmint(const vector<mint>& v){for(const auto& e : v){ cout << e.val() << " "; } cout << endl;}
+template<typename T> void outvv(const vector<vector<T>>& vv){for(const auto& v : vv){ outv(v); }}
+void outvvmint(const vector<vector<mint>>& vv){for(const auto& v : vv){ outvmint(v); }}
+template<typename T> void outvp(const vector<pair<T, T>>& vp){for(const auto& p : vp){ cout << p.first << " " << p.second << endl; }}
+
+int main() {
+    ll N, M;
+    cin >> N >> M;
+    vpll AC(M);
+    rep(i, M) {
+        ll a, c;
+        cin >> a >> c;
+        AC.at(i) = {c, a};
+    }
+    sort(AC.begin(), AC.end());
+
+    ll ans = 0;
+    ll g = N;
+    ll count = N;
+    rep(i, M) {
+        ll c = AC.at(i).first;
+        ll a = AC.at(i).second;
+        ll d = gcd(a, count);
+        ans += c * (count - d);
+        count = d;
+    }
+
+    if (count != 1) {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
