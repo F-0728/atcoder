a30f4c6c175440d5fa3e5619a649319661c8f6c8
F-0728 <fugue2767@icloud.com>
2025-02-22 00:32:00 +0900

    solved ABC153-F

---
+++ ABC/F/153F.cpp
@@ -0,0 +1,86 @@
+#include <atcoder/all>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <chrono>
+#include <bitset>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << "\n"
+#define No cout << "No" << "\n"
+ll power(ll x, ll y) {ll res = 1; while(y > 0){if (y % 2 == 1) {res *= x;} x *= x; y >>= 1;}return res;}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+ll pc(ll x) { return __builtin_popcountll(x); }
+void argsort(vll& v, vll& idx) { iota(idx.begin(), idx.end(), 0); sort(idx.begin(), idx.end(), [&v](ll i1, ll i2) { return v[i1] < v[i2]; }); }
+template<typename T> void outv(const vector<T>& v){for(const auto& e : v){ cout << e << " "; } cout << "\n";}
+void outvmint(const vector<mint>& v){for(const auto& e : v){ cout << e.val() << " "; } cout << "\n";}
+template<typename T> void outvv(const vector<vector<T>>& vv){for(const auto& v : vv){ outv(v); }}
+void outvvmint(const vector<vector<mint>>& vv){for(const auto& v : vv){ outvmint(v); }}
+template<typename T> void outvp(const vector<pair<T, T>>& vp){for(const auto& p : vp){ cout << p.first << " " << p.second << "\n"; }}
+
+using S = long long;
+using F = long long;
+
+S op(S a, S b){ return std::min(a, b); }
+S e(){ return INF; }
+S mapping(F f, S x){ return f+x; }
+F composition(F f, F g){ return f+g; }
+F id(){ return 0; }
+
+int main() {
+    ll N, D, A;
+    cin >> N >> D >> A;
+    vpll XH(N);
+    rep(i, N) {
+        cin >> XH[i].first >> XH[i].second;
+    }
+    sort(XH.begin(), XH.end());
+
+    lazy_segtree<S, op, e, F, mapping, composition, id> seg(N);
+    ll ans = 0;
+    rep(i, N) {
+        seg.set(i, XH[i].second);
+    }
+
+    rep(i, N) {
+        ll x = XH[i].first;
+        ll h = seg.get(i);
+        if (h <= 0) continue;
+        ll cnt = (h + A - 1) / A;
+        ans += cnt;
+        ll damage = cnt * A;
+        ll right = x + 2 * D;
+        ll idx = upper_bound(XH.begin(), XH.end(), pll(right, INF)) - XH.begin();
+        seg.apply(i, idx, -damage);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
