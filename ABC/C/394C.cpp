d6f4c5c2de194bb281ffb123fa8f230d9720122f
F-0728 <fugue2767@icloud.com>
2025-02-22 23:37:49 +0900

    participated in ABC394

---
+++ ABC/C/394C.cpp
@@ -0,0 +1,109 @@
+#include <atcoder/all>
+#include <bitset>
+#include <chrono>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+using namespace std;
+using namespace atcoder;
+
+// using mint = modint1000000007;
+using mint = modint998244353;
+using ll = long long;
+using ld = long double;
+const ll inf = 1 << 30;
+const ll INF = 1LL << 60;
+const vector<pair<ll, ll>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define vll vector<ll>
+#define vld vector<ld>
+#define vbool vector<bool>
+#define vmint vector<mint>
+#define pll pair<ll, ll>
+#define vvll vector<vector<ll>>
+#define vvld vector<vector<ld>>
+#define vvbool vector<vector<bool>>
+#define vvmint vector<vector<mint>>
+#define vpll vector<pair<ll, ll>>
+#define Yes cout << "Yes" << "\n"
+#define No cout << "No" << "\n"
+ll power(ll x, ll y) {
+    ll res = 1;
+    while (y > 0) {
+        if (y % 2 == 1) {
+            res *= x;
+        }
+        x *= x;
+        y >>= 1;
+    }
+    return res;
+}
+ll opsum(ll a, ll b) { return a + b; }
+ll opmax(ll a, ll b) { return max(a, b); }
+ll opmin(ll a, ll b) { return min(a, b); }
+ll esum() { return 0; }
+ll emax() { return -INF; }
+ll emin() { return INF; }
+ll pc(ll x) { return __builtin_popcountll(x); }
+void argsort(vll& v, vll& idx) {
+    iota(idx.begin(), idx.end(), 0);
+    sort(idx.begin(), idx.end(), [&v](ll i1, ll i2) { return v[i1] < v[i2]; });
+}
+template <typename T>
+void outv(const vector<T>& v) {
+    for (const auto& e : v) {
+        cout << e << " ";
+    }
+    cout << "\n";
+}
+void outvmint(const vector<mint>& v) {
+    for (const auto& e : v) {
+        cout << e.val() << " ";
+    }
+    cout << "\n";
+}
+template <typename T>
+void outvv(const vector<vector<T>>& vv) {
+    for (const auto& v : vv) {
+        outv(v);
+    }
+}
+void outvvmint(const vector<vector<mint>>& vv) {
+    for (const auto& v : vv) {
+        outvmint(v);
+    }
+}
+template <typename T>
+void outvp(const vector<pair<T, T>>& vp) {
+    for (const auto& p : vp) {
+        cout << p.first << " " << p.second << "\n";
+    }
+}
+
+int main() {
+    string S;
+    cin >> S;
+    ll N = S.size();
+    rep(i, N - 1) {
+        bool change = false;
+        if (S[i] == 'W' && S[i + 1] == 'A') {
+            S[i] = 'A';
+            S[i + 1] = 'C';
+            change = true;
+        }
+        if (change) {
+            ll idx = i - 1;
+            while (idx >= 0 && S[idx] == 'W') {
+                S[idx] = 'A';
+                S[idx + 1] = 'C';
+                idx--;
+            }
+        }
+    }
+
+    cout << S << endl;
+
+    return 0;
+}
